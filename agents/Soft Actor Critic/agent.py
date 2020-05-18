56b08e9e4d73dd19038cd1452c5d5b29f0f43bd7
Gianluca De Stefano <gianluca.destefano@rub.de>
2020-05-18 22:53:54 +0200

    Implement alpha update; Actor backward pass tbd;

--- agents/Soft Actor Critic/agent.py
+++ agents/Soft Actor Critic/agent.py
@@ -5,6 +5,10 @@ import numpy as np
 from actor import Actor
 from critic import Critic
 from buffers import ReplayBuffer
+from adam import Adam
+
+import autograd.numpy as np
+from autograd import grad
 
 LR_ACTOR = float(5e-4)
 LR_CRITIC = float(5e-4)
@@ -22,8 +26,6 @@ class Agent():
         
         self.target_entropy = -action_size  # -dim(A)
         self.alpha = 1
-        #self.log_alpha = torch.tensor([0.0], requires_grad=True)
-        #self.alpha_optimizer = optim.Adam(params=[self.log_alpha], lr=LR_ACTOR) 
         self._action_prior = action_prior
         
         # Actor Network 
@@ -41,8 +43,10 @@ class Agent():
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
-        
 
+        # Adam Optimizer
+        self.adam = Adam(0.0)
+        
     def step(self, state, action, reward, next_state, done, step):
         self.memory.add(state, action, reward, next_state, done)
 
@@ -54,6 +58,10 @@ class Agent():
     def act(self, state):
         action = self.actor_local.get_action(state)
         return action
+
+    def alpha_loss(self, x, log_pis):
+        loss = -(x * (log_pis + self.target_entropy)).mean()
+        return loss
 
     def learn(self, step, experiences, gamma, d=1):
         """Updates actor, critics and entropy_alpha parameters using given batch of experience tuples.
@@ -98,15 +106,32 @@ class Agent():
 
         #Actor Learning Step
         if step % d == 0:
-            pass
+            alpha = np.exp(self.adam.param)
+            # Compute alpha loss
+            actions_pred, log_pis = self.actor_local.evaluate(states)
 
+            #Forward Pass
+            a_loss = grad(self.alpha_loss)
+            a_grad = a_loss(self.adam.param,log_pis) 
+
+            #Backward Pass
+            self.adam.backward_pass(alpha_loss)
+            self.alpha = alpha
+
+            # policy_prior_log_probs = 0.0
+            # actor_loss = (alpha * log_pis.squeeze - self.critic1(states, actions_pred.squeeze(0)) - policy_prior_log_probs).mean()
+
+            # # Minimize the loss
+            # self.actor_optimizer.zero_grad()
+            # actor_loss.backward()
+            # self.actor_optimizer.step()
+                
         # ----------------------- update target networks ----------------------- #
         self.soft_update(self.critic1.network, self.critic1_target.network, TAU)
         self.soft_update(self.critic2.network, self.critic2_target.network, TAU)
                      
 
     def soft_update(self, local_model, target_model, tau):
-        pass
         """Soft update model parameters.
         θ_target = τ*θ_local + (1 - τ)*θ_target
         """
