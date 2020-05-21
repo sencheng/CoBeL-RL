44944c920003ffb5692e349e5a4060d86e52dad2
Gianluca De Stefano <gianluca.destefano@rub.de>
2020-05-21 14:46:33 +0200

    Bugfix;

--- agents/Soft Actor Critic/agent.py
+++ agents/Soft Actor Critic/agent.py
@@ -33,7 +33,6 @@ class Agent():
         self.alpha = 1
         self.log_alpha = tf.Variable([0.0])
         self.alpha_opt = tf.keras.optimizers.Adam(lr=LR_ACTOR)
-        
         self._action_prior = action_prior
         
         # Actor Network 
@@ -42,6 +41,7 @@ class Agent():
 
         # Critic Network (w/ Target Network)
         self.critic1 = Critic(state_size, action_size, random_seed, hidden_size)
+
         self.critic2 = Critic(state_size, action_size, random_seed, hidden_size)
         
         self.critic1_target = Critic(state_size, action_size, random_seed,hidden_size)
@@ -53,8 +53,7 @@ class Agent():
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
-        self.halving = np.arange(256)
-        self.halving = self.halving.fill(0.5)
+        self.halving = np.full((256,), 0.5, dtype=float)
         
     def step(self, state, action, reward, next_state, done, step):
         self.memory.add(state, action, reward, next_state, done)
@@ -68,15 +67,13 @@ class Agent():
         action = self.actor_local.get_action(state)
         return action
 
-    def alpha_loss(self, x, log_pis):
-        loss = -(x * (log_pis + self.target_entropy)).mean()
-        return loss
-
     def actor_step(self,X,alpha,policy_prior_log_probs):
         with tf.GradientTape() as tape:
             actions_pred , log_pis = self.actor_local.model(X)
             c1_in = self.critic1.predict_learn(X, actions_pred)
+            #print(c1_in.mean())
             loss = K.mean((alpha * log_pis - c1_in - policy_prior_log_probs))
+        #print(loss)
         grads = tape.gradient(loss,self.actor_local.model.trainable_variables)
         self.actor_opt.apply_gradients(zip(grads, self.actor_local.model.trainable_variables))
 
@@ -85,6 +82,7 @@ class Agent():
             tape.watch(self.log_alpha)
             actions_pred, log_pis = self.actor_local.model(X)
             alpha_loss = - K.mean(self.log_alpha.read_value()[0] * (log_pis + self.target_entropy))
+        #print(alpha_loss)
         grads = tape.gradient(alpha_loss,self.log_alpha)
         grads = tf.expand_dims(grads,0)
         self.alpha_opt.apply_gradients(zip(grads, [self.log_alpha]))
@@ -102,8 +100,7 @@ class Agent():
 
         # take the min of both critics for updating
         Q_target_next = np.minimum(Q_target1_next.squeeze(), Q_target2_next.squeeze())
-
-        #next_action = next_action.squeeze()
+        
         log_pis_next = log_pis_next.squeeze()
 
         if FIXED_ALPHA == None:
@@ -142,9 +139,7 @@ class Agent():
                      
 
     def soft_update(self, local_model, target_model, tau):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-        """
-        a = np.array(local_model.get_weights()) 
-        b = np.array(target_model.get_weights()) 
-        target_model.set_weights(b + (1-tau)*a)
+        a = np.array(local_model.get_weights()) #local
+        b = np.array(target_model.get_weights()) #target
+        #98% of target + 2% local
+        target_model.set_weights(tau*a + (1.0-tau)*b)
