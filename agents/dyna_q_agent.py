e517c421eef791d4f8dafc475d5607d24c4c1c95
Nicolas Diekmann <nicolas.diekmann@ruhr-uni-bochum.de>
2021-05-19 13:10:21 +0000

    Updated dyna_q_agent.py Made changes to the Dyna-Q agents. Added PMA agent.

--- agents/dyna_q_agent.py
+++ agents/dyna_q_agent.py
@@ -1,7 +1,7 @@
 # basic imports
 import numpy as np
 # memory module
-from memory_modules.dyna_q_memory import DynaQMemory
+from memory_modules.dyna_q_memory import DynaQMemory, PMAMemory
 
 
 class AbstractDynaQAgent():
@@ -26,6 +26,7 @@ class AbstractDynaQAgent():
         self.beta = beta
         self.gamma = gamma
         self.learningRate = learningRate
+        self.policy = 'greedy'
         # mask invalid actions?
         self.mask_actions = False
         self.compute_action_mask()
@@ -43,19 +44,18 @@ class AbstractDynaQAgent():
         for experience in replayBatch:
             self.update_Q(experience)
         
-    def select_action(self, state, method='greedy', epsilon=0.3, beta=5):
+    def select_action(self, state, epsilon=0.3, beta=5):
         '''
         This function selects an action according to the Q-values of the current state.
         
         | **Args**
         | state:                        The current state.
-        | method:                       The method used for action selection.
         | epsilon:                      The epsilon parameter used under greedy action selection.
         | beta:                         The temperature parameter used when applying the softmax function to the Q-values.
         '''
-        # revert to 'argmax' in case that the method name is not valid
-        if not method in ['greedy', 'softmax']:
-            method = 'greedy'
+        # revert to 'greedy' in case that the method name is not valid
+        if not self.policy in ['greedy', 'softmax']:
+            self.policy = 'greedy'
         # retrieve Q-values
         qVals = self.retrieve_Q(state)
         actions = np.arange(qVals.shape[0])
@@ -64,7 +64,7 @@ class AbstractDynaQAgent():
             qVals = qVals[self.action_mask[state]]
             actions = actions[self.action_mask[state]]
         # select action with highest value
-        if method == 'greedy':
+        if self.policy == 'greedy':
             # act greedily and break ties
             action = np.argmax(qVals)
             ties = np.arange(qVals.shape[0])[(qVals == qVals[action])]
@@ -74,7 +74,7 @@ class AbstractDynaQAgent():
                 action = np.random.randint(qVals.shape[0])
             return actions[action]
         # select action probabilistically
-        elif method == 'softmax':
+        elif self.policy == 'softmax':
             probs = np.exp(beta * qVals)/np.sum(np.exp(beta * qVals))
             action = np.random.choice(qVals.shape[0], p=probs)
             return actions[action]
@@ -174,13 +174,15 @@ class DynaQAgent(AbstractDynaQAgent):
                 
             
     def __init__(self, interfaceOAI, epsilon=0.3, beta=5, learningRate=0.9, gamma=0.99, trialEndFcn=None):
-        super().__init__(interfaceOAI, epsilon=0.3, beta=5, learningRate=0.9, gamma=0.99)
+        super().__init__(interfaceOAI, epsilon=epsilon, beta=beta, learningRate=learningRate, gamma=gamma)
         # Q-table
         self.Q = np.zeros((self.interfaceOAI.world['states'], self.numberOfActions))
         # memory module
         self.M = DynaQMemory(self.interfaceOAI.world['states'], self.numberOfActions)
         # set up the visualizer for the RL agent behavior/reward outcome
         self.engagedCallbacks = self.callbacks(self,trialEndFcn)
+        # perform replay at the end of an episode instead of each step
+        self.episodic_replay = False
         
     def train(self, numberOfTrials=100, maxNumberOfSteps=50, replayBatchSize=100, noReplay=False):
         '''
@@ -199,7 +201,7 @@ class DynaQAgent(AbstractDynaQAgent):
             logs = {'episode_reward': 0}
             for step in range(maxNumberOfSteps):
                 # determine next action
-                action = self.select_action(state, 'greedy', self.epsilon, self.beta)
+                action = self.select_action(state, self.epsilon, self.beta)
                 # perform action
                 next_state, reward, stopEpisode, callbackValue = self.interfaceOAI.step(action)
                 # make experience
@@ -211,13 +213,16 @@ class DynaQAgent(AbstractDynaQAgent):
                 # update current state
                 state = next_state
                 # perform experience replay
-                if not noReplay:
+                if not noReplay and not self.episodic_replay:
                     self.replay(replayBatchSize)
                 # update cumulative reward
                 logs['episode_reward'] += reward
                 # stop trial when the terminal state is reached
                 if stopEpisode:
                     break
+            # perform experience replay
+            if not noReplay and self.episodic_replay:
+                self.replay(replayBatchSize)
             # callback
             self.engagedCallbacks.on_episode_end(trial, logs)
     
@@ -235,6 +240,145 @@ class DynaQAgent(AbstractDynaQAgent):
         # update Q-function with TD-error
         self.Q[experience['state']][experience['action']] += self.learningRate * td
     
+    def retrieve_Q(self, state):
+        '''
+        This function retrieves Q-values for a given state.
+        
+        | **Args**
+        | state:                        The state for which Q-values should be retrieved.
+        '''
+        # retrieve Q-values, if entry exists
+        return self.Q[state]
+    
+    def predict_on_batch(self, batch):
+        '''
+        This function retrieves Q-values for a batch of states.
+        
+        | **Args**
+        | batch:                        The batch of states.
+        '''
+        predictions = []
+        for state in batch:
+            predictions += [self.retrieve_Q(state)]
+            
+        return np.array(predictions)
+    
+    
+class PMAAgent(AbstractDynaQAgent):
+    '''
+    Implementation of a Dyna-Q agent using the Prioritized Memory Access (PMA) method described by Mattar & Daw (2019).
+    
+    | **Args**
+    | interfaceOAI:                 The interface to the Open AI Gym environment.
+    | epsilon:                      The epsilon value for the epsilon greedy policy.
+    | learningRate:                 The learning rate with which the Q-function is updated.
+    | gamma:                        The discount factor used to compute the TD-error.
+    | trialEndFcn:                  The callback function called at the end of each trial, defined for more flexibility in scenario control.
+    '''
+    
+    class callbacks():
+        '''
+        Callback class. Used for visualization and scenario control.
+        
+        | **Args**
+        | rlParent:                     Reference to the Dyna-Q agent.
+        | trialEndFcn:                  Maximum number of experiences that will be stored by the memory module.
+        '''
+
+        def __init__(self, rlParent, trialEndFcn=None):
+            super(PMAAgent.callbacks, self).__init__()
+            # store the hosting class
+            self.rlParent = rlParent
+            # store the trial end callback function
+            self.trialEndFcn = trialEndFcn
+        
+        def on_episode_end(self, epoch, logs):
+            '''
+            The following function is called whenever an episode ends, and updates the reward accumulator,
+            simultaneously updating the visualization of the reward function.
+            
+            | **Args**
+            | rlParent:                     Reference to the Dyna-Q agent.
+            | trialEndFcn:                  Maximum number of experiences that will be stored by the memory module.
+            '''
+            if self.trialEndFcn is not None:
+                self.trialEndFcn(epoch, self.rlParent, logs)
+                
+            
+    def __init__(self, interfaceOAI, epsilon=0.3, beta=5, learningRate=0.9, gamma=0.99, trialEndFcn=None, gammaSR=0.99):
+        super().__init__(interfaceOAI, epsilon=epsilon, beta=beta, learningRate=learningRate, gamma=gamma)
+        # Q-table
+        self.Q = np.zeros((self.interfaceOAI.world['states'], self.numberOfActions))
+        # memory module
+        self.M = PMAMemory(self.interfaceOAI, self, self.interfaceOAI.world['states'], self.numberOfActions, gammaSR)
+        # set up the visualizer for the RL agent behavior/reward outcome
+        self.engagedCallbacks = self.callbacks(self,trialEndFcn)
+        # replay traces
+        self.replay_traces = {'start': [], 'end': []}
+        # logging
+        self.rewards = []
+        
+    def train(self, numberOfTrials=100, maxNumberOfSteps=50, replayBatchSize=100, noReplay=False):
+        '''
+        This function is called to train the agent.
+        
+        | **Args**
+        | numberOfTrials:               The number of trials the Dyna-Q agent is trained.
+        | maxNumberOfSteps:             The maximum number of steps per trial.
+        | replayBatchSize:              The number of random that will be replayed.
+        | noReplay:                     If true, experiences are not replayed.
+        '''
+        for trial in range(numberOfTrials):
+            # reset environment
+            state = self.interfaceOAI.reset()
+            # log cumulative reward
+            logs = {'episode_reward': 0}
+            for step in range(maxNumberOfSteps):
+                # determine next action
+                action = self.select_action(state, self.epsilon, self.beta)
+                # perform action
+                next_state, reward, stopEpisode, callbackValue = self.interfaceOAI.step(action)
+                # make experience
+                experience = {'state': state, 'action': action, 'reward': reward, 'next_state': next_state, 'terminal': (1 - stopEpisode)}
+                # update Q-function with experience
+                self.update_Q([experience])
+                # store experience
+                self.M.store(experience)
+                # update current state
+                state = next_state
+                # update cumulative reward
+                logs['episode_reward'] += reward
+                # stop trial when the terminal state is reached
+                if stopEpisode:
+                    break
+            # perform experience replay
+            if not noReplay:
+                self.M.updateSR()
+                self.replay_traces['end'] += [self.M.replay(replayBatchSize, next_state)]
+            self.rewards += [logs['episode_reward']]
+            # callback
+            self.engagedCallbacks.on_episode_end(trial, logs)
+        
+    def update_Q(self, update):
+        '''
+        This function updates the Q-function with a given experience.
+        
+        | **Args**
+        | experience:                   The experience with which the Q-function will be updated.
+        '''
+        # expected future value
+        future_value = np.amax(self.Q[update[-1]['next_state']]) * update[-1]['terminal']
+        for s, step in enumerate(update):
+            # sum rewards over remaining trajectory
+            R = 0.
+            for following_steps in range(len(update) - s):
+                R += update[s + following_steps]['reward'] * (self.gamma ** following_steps)
+            # compute TD-error
+            td = R + future_value * (self.gamma ** (following_steps + 1))
+            td -= self.retrieve_Q(step['state'])[step['action']]
+            # update Q-function with TD-error
+            self.Q[step['state']][step['action']] += self.learningRate * td
+        
     def retrieve_Q(self, state):
         '''
         This function retrieves Q-values for a given state.
