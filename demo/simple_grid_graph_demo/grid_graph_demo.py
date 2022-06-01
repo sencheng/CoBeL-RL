01e7f5356da81f7ecfcd0af2192f6ee036f0efaa
Sandhiya Vijayabaskaran <vijaysby@zimbra.ini.ruhr-uni-bochum.de>
2022-06-01 15:08:30 +0200

    rectangular graph demo

---
+++ demo/simple_grid_graph_demo/grid_graph_demo.py
@@ -0,0 +1,171 @@
+
+import sys
+import shutil
+import os
+import multiprocessing as mp
+import numpy as np
+import time
+
+import pyqtgraph as qg
+
+from multiprocessing import Pool, Process
+from pathlib import Path
+from numpy import random
+from tensorflow.python.keras import backend
+
+########  Adding Parent directory path to system, so that  Module files can be accessed ######## 
+
+try:
+    sys.path.append(os.path.abspath(__file__ + "/../../../"))
+except IndexError:
+    pass
+
+########
+
+
+from frontends.frontends_blender import FrontendBlenderInterface
+from spatial_representations.topology_graphs.simple_topology_graph import GridGraph
+from agents.dqn_agents import DQNAgentBaseline
+from observations.image_observations import ImageObservationBaseline
+from interfaces.oai_gym_interface import OAIGymInterface
+from analysis.rl_monitoring.rl_performance_monitors import RLPerformanceMonitorBaseline
+
+# shall the system provide visual output while performing the experiments?
+# NOTE: do NOT use visualOutput=True in parallel experiments,
+# visualOutput=True should only be used in explicit calls to 'singleRun'!
+visualOutput = True
+
+def rewardCallback(values):
+    """
+    This is a callback function that defines the reward provided to the robotic agent.
+    Note: this function has to be adopted to the current experimental design.
+    This is flexible enough to accommodate for different experimental setups.
+
+    Parameters:
+    ----------
+    values: a dict of values that are transferred from the OAI module to the reward function.
+
+    Returns:
+    --------
+    reward : reward value
+    stopEpisode: flag set to true/false
+    """
+
+    rlAgent = values['rlAgent']
+    # the standard reward for each step taken is negative, making the agent seek short routes
+    reward = -1.0
+
+    stopEpisode = False
+
+    if values['currentNode'].goalNode:
+        reward = 10.0
+        stopEpisode = True
+
+    return [reward, stopEpisode]
+
+
+
+def trialBeginCallback(trial, rlAgent):
+    """
+    This is a callback function that is called in the beginning of each trial.
+    Here, experimental behavior can be defined (ABA renewal and the like).
+
+    Parameters:
+    -----------
+    trial:      the number of the finished trial
+    rlAgent:    the employed reinforcement learning agent
+
+    Returns:
+    --------
+    None
+    """
+    if trial == rlAgent.trialNumber - 1:
+        # end the experiment by setting the number of steps to a excessively large value, this stops the 'fit' routine
+        rlAgent.agent.step = rlAgent.maxSteps + 1
+
+
+
+
+def trialEndCallback(trial, rlAgent, logs):
+    """
+    This is a callback routine that is called when a single trial ends.
+    Here, functionality for performance evaluation can be introduced.
+
+    Parameters:
+    ----------
+    trial: the number of the finished trial
+    rlAgent: the employed reinforcement learning agent
+    logs: output of the reinforcement learning subsystem
+
+    Returns:
+    -------
+    None
+    """
+    if visualOutput:
+        # update the visual elements if required
+        #TODO : check if this is a general dependency
+        rlAgent.interfaceOAI.modules['spatial_representation'].update_visual_elements()
+        rlAgent.performanceMonitor.update(trial, logs)
+
+
+def singleRun():
+    """
+    This method performs a single experimental run, i.e. one experiment. It has to be called by either a
+    parallelization mechanism (without visual output), or by a direct call (in this case, visual output can be used).
+
+    combinations:  this is a combination of parameters used for a single experiment.
+    Note: the combination values depend on the experimental design!
+
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    -------
+    None
+    """
+    np.random.seed()
+
+    # this is the main window for visual output
+    # normally, there is no visual output, so there is no need for an output window
+    mainWindow = None
+    # if visual output is required, activate an output window
+    if visualOutput:
+        mainWindow = qg.GraphicsWindow(title="workingTitle_Framework")
+
+    # a dictionary that contains all employed modules
+    modules = dict()
+    # world module: Initiates Blender and returns environment limits, robot pose
+    modules['world'] = FrontendBlenderInterface('simple_grid_graph_env/simple_grid_graph_maze.blend')
+    # Observation module: bring the observations of the environment
+    modules['observation'] = ImageObservationBaseline(modules['world'], mainWindow, visualOutput)
+    # spatial obs: pass world instance
+    modules['spatial_representation'] = GridGraph(start_nodes=[0], goal_nodes=[15],
+           visual_output=True,world_module=modules['world'],use_world_limits=True,
+           observation_module=modules['observation'],rotation=False)
+    modules['spatial_representation'].set_visual_debugging(mainWindow)
+    modules['rl_interface'] = OAIGymInterface(modules, visualOutput, rewardCallback)
+
+    rlAgent = DQNAgentBaseline(modules['rl_interface'], 5000, 0.3, None, trialBeginCallback, trialEndCallback)
+
+    # set the experimental parameters
+    rlAgent.trialNumber = 100
+
+    perfMon = RLPerformanceMonitorBaseline(rlAgent, rlAgent.trialNumber, mainWindow, visualOutput)
+    rlAgent.performanceMonitor = perfMon
+
+    # eventually, allow the OAI class to access the robotic agent class
+    modules['rl_interface'].rlAgent = rlAgent
+
+    # and allow the topology class to access the rlAgent
+    modules['spatial_representation'].rlAgent = rlAgent
+
+    # let the agent learn, with extremely large number of allowed maximum steps
+    rlAgent.train(100000)
+
+    backend.clear_session()
+    modules['world'].stopBlender()
+
+
+if __name__ == "__main__":
+    singleRun()
--- spatial_representations/topology_graphs/simple_topology_graph.py
+++ spatial_representations/topology_graphs/simple_topology_graph.py
@@ -68,13 +68,13 @@ class GridGraph(AbstractTopologyGraph) :
         self.world_module = world_module
         self.x_limits = x_limits
         self.y_limits = y_limits
+        self.use_world_limits = use_world_limits
         
         self.observation_module = observation_module
-        #store the graph parameters
-        self.cliqueSize=self.cliqueSize
         
         if self.world_module is not None : 
             self.world_limits = self.world_module.getLimits()
+            self.world_nodes, self.world_edges = world_module.getWallGraph()
             if self.use_world_limits : 
                 self.x_limits = [self.world_limits[0,0], self.world_limits[0,1]]
                 self.y_limits = [self.world_limits[1,0], self.world_limits[1,1]]
@@ -231,9 +231,15 @@ class GridGraph(AbstractTopologyGraph) :
             if self.visual_output : 
                 self.update_position_marker([next_node_pos[0], next_node_pos[1],
                                              0.0, 1.0])
-                if qt.QtGui.QApplication.instance() is not None:
-                    qt.QtGui.QApplication.instance().processEvents()
-        else : 
+                if hasattr(qt.QtGui, 'QApplication'):
+                    if qt.QtGui.QApplication.instance() is not None:
+                        qt.QtGui.QApplication.instance().processEvents()
+                else:
+                    if qt.QtWidgets.QApplication.instance() is not None:
+                        qt.QtWidgets.QApplication.instance().processEvents()
+        
+        else : #ROTATION IS ENABLED
+            
             if action!='reset':                   
                 if self.world_module is not None : 
                     heading = np.array([self.world_module.envData['poseData'][2],
@@ -281,7 +287,7 @@ class GridGraph(AbstractTopologyGraph) :
                 right_edges=sorted(right_edges,key=lambda element: element[2],reverse=True)
                 
                 # store the current node as previous node
-                previous_node=self.currentNode
+                previous_node=self.current_node
             
             
             # with action given, the next node can be computed
@@ -292,49 +298,49 @@ class GridGraph(AbstractTopologyGraph) :
                     
                 if len(forward_edge)!=0:
                     # there is a forward edge that the agent can use
-                    self.nextNode=forward_edge[0]
+                    self.next_node=forward_edge[0]
                     
-                    nextNodePos=np.array([self.nodes[self.nextNode].x,self.nodes[self.nextNode].y])
+                    nextNodePos=np.array([self.nodes[self.next_node].x,self.nodes[self.next_node].y])
                     
             
                 else:
                     # no forward edge found, the agent has to wait for a rotation action
-                    self.nextNode=self.currentNode
-                    nextNodePos=np.array([self.nodes[self.nextNode].x,self.nodes[self.nextNode].y])
+                    self.next_node=self.current_node
+                    nextNodePos=np.array([self.nodes[self.next_node].x,self.nodes[self.next_node].y])
                     
-                self.updateRobotPose([nextNodePos[0],nextNodePos[1],heading[0],heading[1]])
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
+                self.update_position_marker([nextNodePos[0],nextNodePos[1],heading[0],heading[1]])
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
                 
                 
             if action==1:
                 # this is a left turn movement
-                self.nextNode=self.currentNode
-                nextNodePos=np.array([self.nodes[self.nextNode].x,self.nodes[self.nextNode].y])
+                self.next_node=self.current_node
+                nextNodePos=np.array([self.nodes[self.next_node].x,self.nodes[self.next_node].y])
                     
                 angle=180.0/np.pi*np.arctan2(left_edges[0][1][1],left_edges[0][1][0])
-                self.updateRobotPose([nextNodePos[0],nextNodePos[1],left_edges[0][1][0],left_edges[0][1][1]])
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
+                self.update_position_marker([nextNodePos[0],nextNodePos[1],left_edges[0][1][0],left_edges[0][1][1]])
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
                 
             if action==2:
                 # this is a right turn movement
-                self.nextNode=self.currentNode
-                nextNodePos=np.array([self.nodes[self.nextNode].x,self.nodes[self.nextNode].y])
+                self.next_node=self.current_node
+                nextNodePos=np.array([self.nodes[self.next_node].x,self.nodes[self.next_node].y])
                 angle=180.0/np.pi*np.arctan2(right_edges[0][1][1],right_edges[0][1][0])
-                self.updateRobotPose([nextNodePos[0],nextNodePos[1],right_edges[0][1][0],right_edges[0][1][1]])
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
-                self.modules['world'].actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
+                self.update_position_marker([nextNodePos[0],nextNodePos[1],right_edges[0][1][0],right_edges[0][1][1]])
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle])) 
+                self.world_module.actuateRobot(np.array([nextNodePos[0],nextNodePos[1],angle]))
             
             if self.observation_module is not None : 
-                self.modules['observation'].update()
+                self.observation_module.update()
             
             # make the current node the one the agent travelled to
-            self.currentNode=self.nextNode
+            self.current_node=self.next_node
         
             
             # here, next node is already set and the current node is set to this next node.
-            callback_value['currentNode']=self.nodes[self.nextNode]
+            callback_value['currentNode']=self.nodes[self.next_node]
             pass
         
         return callback_value
@@ -347,14 +353,12 @@ class GridGraph(AbstractTopologyGraph) :
             return gym.spaces.Discrete(3)
         #if rotation is False : 
             #pick neighbor
-        return gym.spaces.Discrete(self.n_neighbors)
+        else :
+            return gym.spaces.Discrete(self.n_neighbors)
 
-        
-        
-            
+                
     def set_visual_debugging(self, gui_parent) :
-        self.gui_parent=gui_parent
-        self.visual_output=visual_output
+        self.gui_parent = gui_parent
         self.init_visual_elements()
     
     def init_visual_elements(self) :
@@ -362,12 +366,11 @@ class GridGraph(AbstractTopologyGraph) :
         # iff visualOutput is set to True!
         if self.visual_output:
 
-            # add the graph plot to the GUI widget
             self.plot = self.gui_parent.addPlot(title='Topology graph')
             # set extension of the plot, lock aspect ratio
             if self.use_world_limits :
-                self.plot.setXRange(self.x_limits)
-                self.plot.setYRange(self.y_limits)
+                self.plot.setXRange(self.x_limits[0], self.x_limits[1])
+                self.plot.setYRange(self.y_limits[0], self.y_limits[1])
                 self.plot.setAspectLocked()
 
             # set up indicator arrows for each node, except the goal node, and all nodes in active shock zones iff shock zones exist
@@ -416,11 +419,11 @@ class GridGraph(AbstractTopologyGraph) :
                 tempEdges), symbolBrush=symbolBrushes)
 
             # eventually, overlay robot marker
-            self.posMarker = CogArrow(
+            self.position_marker = CogArrow(
                 angle=0.0, headLen=20.0, tipAngle=25.0, tailLen=0.0, brush=(255, 0, 0))
-            self.plot.addItem(self.posMarker)
+            self.plot.addItem(self.position_marker)
             # initial position to center, this has to be worked over later!
-            self.posMarker.setData(0.0, 0.0, 0.0)
+            self.position_marker.setData(0.0, 0.0, 0.0)
     
     def update_visual_elements(self) :
         '''
@@ -436,45 +439,43 @@ class GridGraph(AbstractTopologyGraph) :
         '''
        
         #somehow remove direct dependence on rlAgent
-        self.sample_state_space() 
-            
-        if self.visual_output:
-            # for all nodes in the topology graph
-            #TODO : sample state space here
-            for node in self.nodes:
-                # query the model at each node's position
-                # only for valid nodes!
-                if node.index!=-1:
-                    observation_module=self.state_space[node.index]
-                    data=np.array([[observation_module]])
-                    # get the q-values at the queried node's position# 
-                    #TODO remove dependency from rlAgent
-                    #q_values = self.rlAgent.agent.model.predict_on_batch(data)[0]
-                    
-                    # find all neighbors that are actually valid (index != -1)
-                    validIndex=0
-                    for n_index in range(len(node.neighbors)):
-                        if node.neighbors[n_index].index!=-1:
-                            validIndex=n_index
-                    
-                    # find the index of the neighboring node that is 'pointed to' by the highest q-value, AND is valid!
-                    maxNeighNode=node.neighbors[np.argmax(q_values[0:validIndex+1])]
-                    # find the direction of the selected neighboring node
-                    # to node: maxNeighNode
-                    toNode=np.array([maxNeighNode.x,maxNeighNode.y])
-                    # from node: node
-                    fromNode=np.array([node.x,node.y])
-                    # the difference vector between to and from
-                    vec=toNode-fromNode
+        if not self.rotation : 
+            self.sample_state_space() 
                 
-                    l=np.linalg.norm(vec)
-                    vec=vec/l
-                    # make the corresponding indicator point in the direction of the difference vector
-                    node.qIndicator.setData(node.x,node.y,np.rad2deg(np.arctan2(vec[1],vec[0])))  
-            self.posMarker.setData(pose[0], pose[1], np.rad2deg(np.arctan2(pose[3], pose[2])))
-
-        
+            if self.visual_output:
+                # for all nodes in the topology graph
+                #TODO : sample state space here
+                for node in self.nodes:
+                    # query the model at each node's position
+                    # only for valid nodes!
+                    if node.index!=-1:
+                        observation_module=self.state_space[node.index]
+                        data=np.array([[observation_module]])
+                        # get the q-values at the queried node's position# 
+                        #TODO remove dependency from rlAgent
+                        q_values = self.rlAgent.agent.model.predict_on_batch(data)[0]                    
+                        # find all neighbors that are actually valid (index != -1)
+                        validIndex=0
+                        for n_index in range(len(node.neighbors)):
+                            if node.neighbors[n_index].index!=-1:
+                                validIndex=n_index
+                        
+                        # find the index of the neighboring node that is 'pointed to' by the highest q-value, AND is valid!
+                        maxNeighNode=node.neighbors[np.argmax(q_values[0:validIndex+1])]
+                        # find the direction of the selected neighboring node
+                        # to node: maxNeighNode
+                        toNode=np.array([maxNeighNode.x,maxNeighNode.y])
+                        # from node: node
+                        fromNode=np.array([node.x,node.y])
+                        # the difference vector between to and from
+                        vec=toNode-fromNode
+                    
+                        l=np.linalg.norm(vec)
+                        vec=vec/l
+                        # make the corresponding indicator point in the direction of the difference vector
+                        node.qIndicator.setData(node.x,node.y,np.rad2deg(np.arctan2(vec[1],vec[0])))  
     
+
     def sample_state_space(self):
         '''
         This function fetches the image data from world module for each node 
@@ -489,33 +490,24 @@ class GridGraph(AbstractTopologyGraph) :
         --------
         state of the agent i.e. list of images from each node
         '''
-        pass
         # TODO : test what this does
         # the world module is required here
         #returns [timeData,poseData,sensorData,imageData]
-        #only imageData required
-        world_module=self.modules['world']
-        
-        #Observation module uses imageData from world module to update the 
-        #observation
-        observation_module=self.modules['observation']
-        
-        # In this specific topology graph, a state is an image sampled from a 
-        #specific node of the graph. There
-        # is no rotation, so one image per node is sufficient.
-        self.state_space = []
-        for node_index in range(len(self.nodes)):
-            #update each node
-            node = self.nodes[node_index]
-            #retrive image data for each node from the camera
-            world_module.step_simulation_without_physics(node.x,node.y,90.0)
-            #udate observation for every node with the imageData
-            observation_module.update()
-            observation=observation_module.observation
-            #create a list of imageData for each node
-            self.state_space+=[observation]
-        #return list of state space
-        return
+
+        if self.world_module is not None and self.observation_module is not None : 
+            self.state_space = []
+            for node_index in range(len(self.nodes)):
+                #update each node
+                node = self.nodes[node_index]
+                #retrive image data for each node from the camera
+                self.world_module.step_simulation_without_physics(node.x,node.y,90.0)
+                #udate observation for every node with the imageData
+                self.observation_module.update()
+                observation=self.observation_module.observation
+                #create a list of imageData for each node
+                self.state_space+=[observation]
+            #return list of state space
+            return
         
         
         
