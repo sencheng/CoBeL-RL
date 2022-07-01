27199b030fbfe4695ba76c5d9890deae5e4a4a73
vijaysby <sandhiya.vijayabaskaran@ruhr-uni-bochum.de>
2022-07-01 12:55:31 +0200

    modified dqn agents to separately compile agent

---
+++ agents/multi_dict_dqn.py
@@ -0,0 +1,144 @@
+from rl.agents import DQNAgent
+import numpy as np
+from dqn_agents import DQNAgentBaseline
+from rl.memory import SequentialMemory
+from rl.policy import EpsGreedyQPolicy
+from tensorflow.keras.optimizers import Adam
+
+class DQNMultiModal(DQNAgent) :
+    '''
+    Slightly changed the backward pass to handle dictionary observations.
+    '''
+    def backward(self, reward, terminal):
+        # Store most recent experience in memory.
+        if self.step % self.memory_interval == 0:
+            self.memory.append(self.recent_observation, self.recent_action, reward, terminal,
+                               training=self.training)
+
+        metrics = [np.nan for _ in self.metrics_names]
+        if not self.training:
+            # We're done here. No need to update the experience memory since we only use the working
+            # memory to obtain the state over the most recent observations.
+            return metrics
+
+        # Train the network on a single stochastic batch.
+        if self.step > self.nb_steps_warmup and self.step % self.train_interval == 0:
+            experiences = self.memory.sample(self.batch_size)
+            assert len(experiences) == self.batch_size
+
+            # Start by extracting the necessary parameters (we use a vectorized implementation).
+            state0_batch = []
+            reward_batch = []
+            action_batch = []
+            terminal1_batch = []
+            state1_batch = []
+            for e in experiences:
+                state0_batch.append(e.state0)
+                state1_batch.append(e.state1)
+                reward_batch.append(e.reward)
+                action_batch.append(e.action)
+                terminal1_batch.append(0. if e.terminal1 else 1.)
+
+            # Prepare and validate parameters.
+            state0_batch = self.process_state_batch(state0_batch)
+            state1_batch = self.process_state_batch(state1_batch)
+            terminal1_batch = np.array(terminal1_batch)
+            reward_batch = np.array(reward_batch)
+            assert reward_batch.shape == (self.batch_size,)
+            assert terminal1_batch.shape == reward_batch.shape
+            assert len(action_batch) == len(reward_batch)
+
+            # Compute Q values for mini-batch update.
+            if self.enable_double_dqn:
+                # According to the paper "Deep Reinforcement Learning with Double Q-learning"
+                # (van Hasselt et al., 2015), in Double DQN, the online network predicts the actions
+                # while the target network is used to estimate the Q value.
+                q_values = self.model.predict_on_batch(state1_batch)
+                assert q_values.shape == (self.batch_size, self.nb_actions)
+                actions = np.argmax(q_values, axis=1)
+                assert actions.shape == (self.batch_size,)
+
+                # Now, estimate Q values using the target network but select the values with the
+                # highest Q value wrt to the online model (as computed above).
+                target_q_values = self.target_model.predict_on_batch(state1_batch)
+                assert target_q_values.shape == (self.batch_size, self.nb_actions)
+                q_batch = target_q_values[range(self.batch_size), actions]
+            else:
+                # Compute the q_values given state1, and extract the maximum for each sample in the batch.
+                # We perform this prediction on the target_model instead of the model for reasons
+                # outlined in Mnih (2015). In short: it makes the algorithm more stable.
+                target_q_values = self.target_model.predict_on_batch(state1_batch)
+                assert target_q_values.shape == (self.batch_size, self.nb_actions)
+                q_batch = np.max(target_q_values, axis=1).flatten()
+            assert q_batch.shape == (self.batch_size,)
+
+            targets = np.zeros((self.batch_size, self.nb_actions))
+            dummy_targets = np.zeros((self.batch_size,))
+            masks = np.zeros((self.batch_size, self.nb_actions))
+
+            # Compute r_t + gamma * max_a Q(s_t+1, a) and update the target targets accordingly,
+            # but only for the affected output units (as given by action_batch).
+            discounted_reward_batch = self.gamma * q_batch
+            # Set discounted reward to zero for all states that were terminal.
+            discounted_reward_batch *= terminal1_batch
+            assert discounted_reward_batch.shape == reward_batch.shape
+            Rs = reward_batch + discounted_reward_batch
+            for idx, (target, mask, R, action) in enumerate(zip(targets, masks, Rs, action_batch)):
+                target[action] = R  # update action with estimated accumulated reward
+                dummy_targets[idx] = R
+                mask[action] = 1.  # enable loss for this specific action
+            targets = np.array(targets).astype('float32')
+            masks = np.array(masks).astype('float32')
+
+            # Finally, perform a single update on the entire batch. We use a dummy target since
+            # the actual loss is computed in a Lambda layer that needs more complex input. However,
+            # it is still useful to know the actual target to compute metrics properly.
+            #based on PR : https://github.com/keras-rl/keras-rl/pull/195/files
+            
+            if isinstance(state0_batch, dict):
+                state0_batch.update({'y_true': targets, 'mask': masks})
+                ins = state0_batch
+            else:
+                ins = [state0_batch] if type(self.model.input) is not list else state0_batch
+                ins += [targets, masks]
+            metrics = self.trainable_model.train_on_batch(ins, [dummy_targets, targets])
+            
+            metrics = [metric for idx, metric in enumerate(metrics) if idx not in (1, 2)]  # throw away individual losses
+            metrics += self.policy.metrics
+            
+            if self.processor is not None:
+                metrics += self.processor.metrics
+
+        if self.target_model_update >= 1 and self.step % self.target_model_update == 0:
+            self.update_target_model_hard()
+
+        return metrics
+
+class DQNAgentMultiModal(DQNAgentBaseline) : 
+    
+        def __init__(self, interface_OAI, memory_capacity=1000000, epsilon=0.3, model=None, custom_callbacks={}): 
+            
+            super().__init__(interface_OAI, memory_capacity=1000000, epsilon=0.3, model=None, custom_callbacks={})
+        # build model
+            
+            self.model = model
+            if self.model is None:
+                self.build_model()
+            # prepare the memory for the RL agent
+            self.memory = SequentialMemory(limit=memory_capacity, window_length=1)
+            # define the available policies
+            policy = EpsGreedyQPolicy(epsilon)
+            # define the maximum number of steps
+            self.max_steps = 10**10
+            # keeps track of current trial
+            self.current_trial = 0 # trial count across all sessions (i.e. calls to the train/simulate method)
+            self.session_trial = 0 # trial count in current seesion (i.e. current call to the train/simulate method)
+            # define the maximum number of trials
+            self.max_trials = 0
+            # construct the agent
+            self.agent = DQNAgent(model=self.model, nb_actions=self.number_of_actions, memory=self.memory, gamma=0.8, nb_steps_warmup=100, enable_dueling_network=False,
+                                dueling_type='avg', target_model_update=1e-2, policy=policy, batch_size=32)
+            # compile the agent
+            self.agent.compile(Adam(lr=.001,), metrics=['mse'])
+            # set up the visualizer for the RL agent behavior/reward outcome
+            self.engaged_callbacks = self.callbacksDQN(self, custom_callbacks)
