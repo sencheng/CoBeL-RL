3b1aaca1860ed06384aabec9aea6dd1b4fa7ee85
Sandhiya Vijayabaskaran <sandhiya.vijayabaskaran@ruhr-uni-bochum.de>
2022-07-28 13:56:29 +0200

    Gitlab ci

---
+++ unit_tests/gridworld_pma_test.py
@@ -0,0 +1,63 @@
+# basic imports
+import sys, getopt
+import numpy as np
+import pyqtgraph as qg
+# CoBel-RL framework
+from cobel.agents.dyna_q_agent import PMAAgent
+from cobel.interfaces.oai_gym_gridworlds import OAIGymInterface
+from cobel.misc.gridworld_tools import makeOpenField
+
+# shall the system provide visual output while performing the experiments?
+# NOTE: do NOT use visualOutput=True in parallel experiments, visualOutput=True should only be used in explicit calls to 'singleRun'! 
+
+
+def single_run():
+    '''
+    This method performs a single experimental run, i.e. one experiment.
+    It has to be called by either a parallelization mechanism (without visual output),
+    or by a direct call (in this case, visual output can be used).
+    '''
+    np.random.seed()
+    
+    visual_output = False
+    # normally, there is no visual output, so there is no need for an output window
+    main_window = None
+    # if visual output is required, activate an output window
+    if visual_output:
+        main_window = qg.GraphicsWindow(title='Demo: PMA Agent')
+    
+    # initialize world            
+    world = makeOpenField(1, 10, 9, 10)
+    world['startingStates'] = np.array([0])
+    
+    # a dictionary that contains all employed modules
+    modules = {}
+    modules['rl_interface'] = OAIGymInterface(modules, world, visual_output, main_window)
+    
+    # amount of trials
+    number_of_trials = 100
+    # maximum steos per trial
+    max_steps = 30
+        
+    # initialize RL agent
+    rl_agent = PMAAgent(interface_OAI=modules['rl_interface'], epsilon=0.3, beta=5,
+                        learning_rate=0.9, gamma=0.9)
+    
+    # initialize experiences
+    for state in range(10):
+        for action in range(4):
+            rl_agent.M.states[state, action] = np.argmax(world['sas'][state, action])
+    rl_agent.M.compute_update_mask()
+    
+    # eventually, allow the OAI class to access the robotic agent class
+    modules['rl_interface'].rl_agent = rl_agent
+    
+    # let the agent learn
+    rl_agent.train(number_of_trials, max_steps, replay_batch_size=5)
+    
+    # and also stop visualization
+    if visual_output:
+        main_window.close()
+
+if __name__ == '__main__':
+    single_run()
